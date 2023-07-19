3ac102cabecf7f8a9b068bdc20dfa8af196e5a0f
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-07-19 19:18:59 +0100

    Added maps div

--- client/src/components/PlayDaily.tsx
+++ client/src/components/PlayDaily.tsx
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 
 export const PlayDaily = () => {
   const { state } = useLocation();
+  const streetviewDivRef = useRef<HTMLDivElement | null>(null);
   const mapDivRef = useRef<HTMLDivElement | null>(null);
   const { currentChallenge, challengeSubmission } = state;
   const [questions, setQuestions] = useState(
@@ -16,18 +17,32 @@ export const PlayDaily = () => {
     // console.log("Challenge data:", currentChallenge);
     // console.log("Submission data:", challengeSubmission);
     // console.log("Questions:", questions);
-    console.log(questions[0].correctPos[0].lat);
 
-    const fenway = { lat: 42.345573, lng: -71.098326 };
+    const coords = {
+      lat: questions[0].correctPos[0].lat,
+      lng: questions[0].correctPos[0].lng,
+    };
 
-    // const mapOptions = {
-    //   center: {
-    //     lat: questions[0].correctPos[0].lat,
-    //     lng: questions[0].correctPos[0].lng,
-    //   },
-    //   zoom: 11,
-    //   streetViewControl: true,
-    // };
+    const streetViewOptions = {
+      position: coords,
+      addressControl: false,
+      motionTracking: false,
+      motionTrackingControl: false,
+      fullscreenControl: false,
+      showRoadLabels: false,
+      zoomControl: false,
+    };
+
+    const mapOptions = {
+      center: { lat: 0, lng: 0 },
+      zoom: 0,
+      clickableIcons: false,
+      disableDefaultIO: true,
+      fullscreenControl: false,
+      streetViewControl: false,
+      mapTypeControl: false,
+      minZoom: 0,
+    };
 
     const loader = new Loader({
       apiKey: import.meta.env.VITE_MAPS_API_KEY,
@@ -37,19 +52,14 @@ export const PlayDaily = () => {
     loader
       .load()
       .then(async (loadedGoogle) => {
-        // const {Map} = await google.maps.importLibrary("maps") as google.maps.MapsLibrary
-        // map = new Map(document.getElementById("map") as HTMLElement, {
-        //   center: coords
-        // })
-        const mapInstance = new loadedGoogle.maps.StreetViewPanorama(
+        const streetViewInstance = new loadedGoogle.maps.StreetViewPanorama(
+          streetviewDivRef.current!,
+          streetViewOptions
+        );
+
+        const mapInstance = new loadedGoogle.maps.Map(
           mapDivRef.current!,
-          {
-            position: fenway,
-            pov: {
-              heading: 34,
-              pitch: 10,
-            },
-          }
+          mapOptions
         );
       })
       .catch((err) => {
@@ -60,7 +70,18 @@ export const PlayDaily = () => {
   return (
     <>
       <h1>Play Daily</h1>
-      <div ref={mapDivRef} style={{ height: "90vh", width: "100%" }}></div>
+
+      <div
+        className="relative"
+        ref={streetviewDivRef}
+        style={{ height: "80vh", width: "100%" }}
+      >
+        <div
+          className="absolute bottom-0 right-0 z-10 h-1/3 w-1/5 m-5"
+          // className="absolute bottom-0 right-0 z-10 h-full w-full m-5"
+          ref={mapDivRef}
+        ></div>
+      </div>
     </>
   );
 };
