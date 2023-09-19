5815e6ed12665c7d988ae4aaeea0652b90648b05
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-09-19 17:10:26 +0100

    Fixed ERR_HTTP_HEADERS_SENT errror in verifyjwt middleware

--- server/middleware/verifyJWT.ts
+++ server/middleware/verifyJWT.ts
@@ -7,16 +7,20 @@ const prisma = new PrismaClient();
 
 const verifyJWT = (req: customRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!(authHeader as string)?.startsWith("Bearer "))
-    return res.sendStatus(401);
+  if (!(authHeader as string)?.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
   const token = (authHeader as string).split(" ")[1];
-  // console.log(token);
 
   jwt.verify(
     token,
     process.env.ACCESS_TOKEN_SECRET,
     async (err: any, decoded: decodedResult) => {
-      if (err) return res.sendStatus(403).json({ error: "Invalid token" }); //invalid token
+      if (err) {
+        console.error("Error inside");
+        console.error(err);
+        return res.status(403).json({ error: "Invalid token" });
+      }
 
       try {
         const user = await prisma.userAccount.findUnique({
@@ -36,11 +40,11 @@ const verifyJWT = (req: customRequest, res: Response, next: NextFunction) => {
         req.email = decoded.email;
         req.roleList = decoded.roleList;
         req.user = user;
+        next();
       } catch (err) {
-        console.log(err);
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
       }
-
-      next();
     }
   );
 };
