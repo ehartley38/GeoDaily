f0c5a0c6e51b1f3e5150df873ec8c3595d3215ab
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-09-20 16:11:03 +0100

    Organised images and moved favicon from static to main images folder

--- client/src/images/images.tsx
+++ client/src/images/images.tsx
@@ -1,8 +1,5 @@
 const IMAGES = {
-  profilePicture: new URL("./ninja.png", import.meta.url).href,
   logo: new URL("./geodailylogo.png", import.meta.url).href,
-  logo2: new URL("./logo2.png", import.meta.url).href,
-  logo3: new URL("./logo3.png", import.meta.url).href,
   addFriendsIcon: new URL("./add_friends_icon.png", import.meta.url).href,
   closeWindow: new URL("./close.png", import.meta.url).href,
   backArrow: new URL("./arrow.png", import.meta.url).href,
