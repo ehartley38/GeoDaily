85611be5b7c8aef5ae52ac664552ece04f6def01
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-09-21 10:58:45 +0100

    Added custom xml2json function which doesnt use require

--- server/utils/getValidStreetView.ts
+++ server/utils/getValidStreetView.ts
@@ -3,27 +3,40 @@ import axios from "axios";
 // let convert = require("xml-js");
 import { config } from "./config.ts";
 
+type ConvertedType = {
+  latt: number;
+  longt: number;
+};
+
 type CoordinatesType = {
   lat: number;
   lng: number;
 };
 
+// https://stackoverflow.com/questions/1773550/convert-xml-to-json-and-back-using-javascript
+function parseXmlToJson(xml: string) {
+  const json: any = {};
+  for (const res of xml.matchAll(
+    /(?:<(\w*)(?:\s[^>]*)*>)((?:(?!<\1).)*)(?:<\/\1>)|<(\w*)(?:\s*)*\/>/gm
+  )) {
+    const key = res[1] || res[3];
+    const value = res[2] && parseXmlToJson(res[2]);
+    json[key] = (value && Object.keys(value).length ? value : res[2]) || null;
+  }
+  return json;
+}
+
 const getRandomPoint = async () => {
   try {
-    // const response = await axios.get(
-    //   "https://api.3geonames.org/?randomland=yes"
-    // );
+    const response = await axios.get(
+      "https://api.3geonames.org/?randomland=yes"
+    );
+    const converted: ConvertedType = parseXmlToJson(response.data);
 
-    // const location = convert.xml2js(response.data, {
-    //   compact: true,
-    //   spaces: 4,
-    // });
+    const lat = converted.latt;
+    const lng = converted.longt;
 
-    // const lat = location.geodata.major.latt._text;
-    // const lng = location.geodata.major.longt._text;
-
-    // return { lat: lat, lng: lng };
-    return { lat: 1, lng: 1 };
+    return { lat: lat, lng: lng };
   } catch (err) {
     console.log(err);
   }
